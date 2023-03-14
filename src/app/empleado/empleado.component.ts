db3b9726851ad9ac4f2c88d79af85eb28dee07b2
jorvesan96 <jorvesan96@gmial.com>
2023-03-14 14:08:20 +0000

    Video 7 Pildoras

---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+  //template:"<p>Aquí iría un empleado</p>",
+  styleUrls: ['./empleado.component.css']
+  //styles:["p{background-color:red;}"]
+})
+export class EmpleadoComponent implements OnInit{
+
+    constructor(){
+
+    }
+
+    ngOnInit(): void{
+
+    }
+}
